d29898af1d02b7f1ab61e7669762f6e9776d9f1c
rndstone <dorppers@gmail.com>
2018-11-26 13:01:42 +0000

    add signup function

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @users = User.new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'users/update'
+
+  get 'users/destroy'
+
   root to: "toppages#index"
+  resources :users, only:[:index, :show, :new, :create]
 end
