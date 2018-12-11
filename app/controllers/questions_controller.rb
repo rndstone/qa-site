42ecc117512f4e1cb76ced820d7abf8836dd4bf7
rndstone <dorppers@gmail.com>
2018-12-11 14:02:27 +0000

    modify to create questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,14 +7,34 @@ class QuestionsController < ApplicationController
       flash[:danger] = 'ログインしてください。'
       redirect_to :login
     end
+    @question = Question.new
   end
 
   def create
+    # puts params[:question]
+    # binding.require 'pry'; binding.pry
+    # category = Category.find(params[:question][:category])
+    # params[:question][:category] = category
+    # puts category
+    @question = current_user.questions.build(question_params)
+    if @question.save
+      flash[:success] = '質問を投稿しました'
+      redirect_to root_url
+    else
+      flash[:danger] = '投稿に失敗しました'
+      render :new
+    end
   end
 
   def show
   end
 
   def destroy
+  end
+  
+  private
+  
+  def question_params
+    params.require(:question).permit(:title, :content, :category_id)
   end
 end
