b6d23f532c54ed912596d125674ca082dd736086
rndstone <dorppers@gmail.com>
2018-12-11 09:07:45 +0000

    modify to require login to post questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,12 @@
 class QuestionsController < ApplicationController
+  before_action :require_user_logged_in, only: [:create, :destroy]
+  include SessionsHelper
+  
   def new
+    unless logged_in?
+      flash[:danger] = 'ログインしてください。'
+      redirect_to :login
+    end
   end
 
   def create
