cff1696a14b626214e24a5276eaaceb0f6438aa2
rndstone <dorppers@gmail.com>
2019-01-10 17:16:26 +0000

    update for best answer

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,7 +2,7 @@ class Answer < ApplicationRecord
   validates :content, presence: true, length: { maximum: 3000 }
   # validate :cannot_answer_closed_question
   include ActiveModel::Validations
-  validates_with ClosedAnswerValidator
+  validates_with ClosedAnswerValidator, on:[:create]
   belongs_to :user
   belongs_to :question
   has_many :answer_likes
@@ -14,4 +14,9 @@ class Answer < ApplicationRecord
   #     errors.add(:answer, "close済みの質問には回答できません")
   #   end
   # end
+  
+  def put_best_answer_first(question_id)
+    answers = Answers.find_by(question_id)
+    answers.order("is_best_answer DESC")
+  end
 end
