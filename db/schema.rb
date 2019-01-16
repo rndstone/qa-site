69fcfcabb5655042e456878067b4ea96424af7ce
rndstone <dorppers@gmail.com>
2019-01-16 15:16:05 +0000

    add upload image function

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190109114351) do
+ActiveRecord::Schema.define(version: 20190114104644) do
 
   create_table "answer_likes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id"
@@ -73,8 +73,9 @@ ActiveRecord::Schema.define(version: 20190109114351) do
     t.string   "name"
     t.string   "email"
     t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                              null: false
+    t.datetime "updated_at",                              null: false
+    t.string   "image_name",      default: "default.jpg"
   end
 
   add_foreign_key "answer_likes", "answers"
