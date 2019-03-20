638a89d2f8a077ab9b28308166948fe0c432cbeb
rndstone <dorppers@gmail.com>
2019-03-20 12:33:54 +0000

    update tests

--- spec/factories/category.rb
+++ spec/factories/category.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :category do
+    id 1
     name '日常生活'
+    # association :question
   end
 end
--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -2,10 +2,11 @@ require 'rails_helper'
 
 RSpec.feature "Users", type: :feature do
   let(:user){FactoryBot.create(:user)}
-  let(:category){FactoryBot.create(:category)}
-  let(:question){FactoryBot.create(:question, user_id:user.id, category_id:category.id)}
-  let(:question2){FactoryBot.create(:question, title:'aaaa',user_id:user.id, category_id:category.id)}
-  describe "ログイン機能" do
+  # let(:category){FactoryBot.create(:category)}
+  # let(:question){FactoryBot.create(:question, user_id:user.id, category_id:category.id)}
+  # let!(:question){FactoryBot.create(:question)}
+  # let(:question2){FactoryBot.create(:question, title:'aaaa',user_id:user.id, category_id:category.id)}
+  # scenario "ログイン機能" do
     it "valid user" do
       # p Question.find(1)
       expect(user).to be_valid
@@ -34,4 +35,4 @@ RSpec.feature "Users", type: :feature do
       expect(page).to have_content "新しい質問"
     end
   end
-end
+# end
