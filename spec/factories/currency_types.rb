2bd191be585d8ebabb40f4a438d34dccd1b7e74d
rndstone <dorppers@gmail.com>
2019-03-24 07:11:05 +0000

    add test for point sysytem

---
+++ spec/factories/currency_types.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :currency_type do
+    id 1
+    name "SanDeGo"
+  end
+end
---
+++ spec/factories/funds.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :fund do
+    amount 1
+    user nil
+  end
+end
